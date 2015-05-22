138a3945ca11bcc0b199a265b907a62f880ff1ec
Masatoshi Nishimura <massa_n@yahoo.ca>
2015-05-22 15:36:34 -0400

    cartesian coordinates length of vector

---
+++ cartesian_cordinates.js
@@ -0,0 +1,6 @@
+function vectorLength(vector){
+	// Formulate is AB = sqrt((x2-x1)^2 + (y2-y1)^2)
+	return Math.sqrt(Math.pow(vector[1][0] - vector[0][0], 2) + Math.pow(vector[1][1]-vector[0][1], 2))
+}
+
+console.log(vectorLength([[0, 3],[4, 0]]));
